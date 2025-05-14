f5a9db61a0fa5d6b9472236af3fb3b486367f8eb
ruuffian <ruuffian@gmail.com>
2025-05-13 22:34:58 -0400

    Now ignores invalid input. Time to take that code and throw it out (abstract into class to allow future extensions).

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cctype>
 #include <iostream>
+#include <limits>
 #include <string>
 
 #include "Menu.h"
@@ -30,22 +31,27 @@ int main() {
   clrscr();
 
   /* Main loop */
-  int choice;
+  int choice = -1;
   std::cout << "Welcome to Paystation!" << '\n' << '\n';
   while (!state.shouldExit) {
     // printPaystationDisplay(ps);
     menu->print();
     // What happens when a non-integer is entered?
-    std::cin >> choice;
-    MenuOption *mo = menu->selectMenuOption(choice);
-    if (mo == NULL) {
-      std::cout << "Invalid input, please try again." << '\n';
-      continue;
+    if (std::cin >> choice) {
+      MenuOption *mo = menu->selectMenuOption(choice);
+      if (mo == NULL) {
+        std::cout << "Invalid input, please try again." << '\n';
+        continue;
+      }
+      mo->execute();
+      /* Ew. */
+      if (mo->getName() == "Exit")
+        state.shouldExit = 1;
+    } else {
+      std::cout << "Input failed. Please enter a valid menu option." << '\n';
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    mo->execute();
-    /* Ew. */
-    if (mo->getName() == "Exit")
-      state.shouldExit = 1;
   }
   return 0;
 }
